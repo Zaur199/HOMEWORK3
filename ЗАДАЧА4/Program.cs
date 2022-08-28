f55de0584abca7aefd0f013ce33d7f0ac7ebcfe4
Zaur199 <zaur.rojer@mail.ru>
2022-08-28 22:18:19 +0300

    Task4

---
+++ ЗАДАЧА4/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+Console.WriteLine("Введите длину");
+int N = int.Parse(Console.ReadLine());
+int count = 1;
+int i = 1;
+while(i <= N)
+{
+    count = (count*i);
+    i++;
+}
+ Console.WriteLine("Произведение чисел в длине равно: " + count);
