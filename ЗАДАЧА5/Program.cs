899c1ef77825cad38667f0e434e1d88cb65e1a23
Zaur199 <zaur.rojer@mail.ru>
2022-08-28 22:18:45 +0300

    Task5

---
+++ ЗАДАЧА5/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать кубы чисел, заканчивающихся на четную цифру от 1 до N
+Console.WriteLine("Введите длину");
+int N = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= N; i++)
+    if(i*i*i % 2 == 0)
+{
+    Console.WriteLine("Кубы чисел, заканчивающихся на четную цифру: " + i*i*i);
+}
