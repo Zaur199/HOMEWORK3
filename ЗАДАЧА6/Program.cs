18c620e4eed352d7cda541bd05bf9aa427983f4e
Zaur199 <zaur.rojer@mail.ru>
2022-08-28 22:19:17 +0300

    TASK6

---
+++ ЗАДАЧА6/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран
+int[]array = {0, 1, 1, 0, 0, 0, 1, 1};
+int index = 0;
+while(index < array.Length)
+{
+    Console.Write($" {array[index]} ");
+    index++;
+}
+
+Console.WriteLine();
