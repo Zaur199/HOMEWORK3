012d4e289d6bc0c3e4456dbe1573c6ebe063afdd
Zaur199 <zaur.rojer@mail.ru>
2022-08-28 22:33:02 +0300

    Task/

---
+++ ЗАДАЧА1/Program.cs
@@ -0,0 +1,7 @@
+﻿//  Найти кубы чисел от 1 до N
+Console.Write("Введите длину: ");
+int N = int.Parse(Console.ReadLine());
+
+for(int i = 1; i <= N; i++)
+    Console.WriteLine(i*i*i + " "); 
+  Console.WriteLine();
